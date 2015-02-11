f946b957167f0414a1f87871e845df17634a2e53
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-02-10 19:09:26 -0500

    Function to browse an item online

---
+++ R/ia_browse.R
@@ -0,0 +1,13 @@
+#' Open an Internet Archive item in the browser
+#' @param item_id The item identifier. If multiple item identifiers are passed
+#'   in, only the first will be opened.
+#' @param type Which page to open: \code{details} is the metadata page,
+#'   \code{stream} is the viewing page for items which are associated with a
+#'   PDF.
+#' @return The item id
+#' @export
+ia_browse <- function(item_id, type = c("details", "stream")) {
+  type <- match.arg(type)
+  browseURL(paste("https://archive.org", type, item_id[1], sep = "/"))
+  return(item_id)
+}
