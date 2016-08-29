3e9985950c7d14d4ded2c5a70d7fd9f3dd003a00
Bob Rietveld <Bob@Bobs-MacBook-Pro.local>
2016-08-29 21:21:48 +0200

    Inital version of wayback addition.

---
+++ R/ia_wayback.R
@@ -0,0 +1,15 @@
+ia_wayback_search <- function(wayback_url, timestamp = NULL) {
+
+  base <- "https://archive.org/"
+  path  <- "wayback/available"
+  query <- list("url" = wayback_url,
+                "timestamp" = timestamp)
+
+  url <-  modify_url(base, path = path, query = query)
+  req <- GET(url)
+  warn_for_status(req)
+  # headers are not application/json so the use of jsonlite is required.
+  response <- jsonlite::fromJSON(content(req,as = "text",encoding = "UTF-8"))
+
+
+}
