fc1302c3d4ae306b25e36cfc5687c716f6044e49
Bob Rietveld <Bob@Bobs-MacBook-Pro.local>
2016-08-30 10:39:49 +0200

    Added documentation

--- R/ia_wayback.R
+++ R/ia_wayback.R
@@ -1,3 +1,17 @@
+
+#' Search the wayback machine
+#'
+#' Provide and url and timestamp and get a url back to the closest snapshot the wayback machine crawled.
+#'
+#' @param wayback_url The url for which to get a snapshot.
+#' @param timestamp timestamp in format YYYYMMDDhhmmss. YYYYMMDD will also work.
+#'
+#' @return List containing the closest snapshot.
+#' @references See the documentation on the Internet Archive's
+#' \href{https://archive.org/help/wayback_api.php}{wayback page}.
+#' @examples
+#' #  wayback <- ia_wayback_search("http://lincolnmullen.com/#software",'20160701005604')
+#' @export
 ia_wayback_search <- function(wayback_url, timestamp = NULL) {
 
   base <- "https://archive.org/"
